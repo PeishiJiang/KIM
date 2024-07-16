2eb6e2fb1c542283d23b625bad206aad24b96421
Jiang, Peishi <peishi.jiang@pnnl.gov>
2024-07-16 14:37:04 -0700

    Added a dataloader.

--- installation.sh
+++ installation.sh
@@ -6,6 +6,14 @@ wait
 pip install joblib
 pip instal hydroeval
 pip install pytest
+pip install equinox
+
+# CPU-only (Linux/macOS/Windows)
+pip install -U jax
+# GPU (NVIDIA, CUDA 12)
+# pip install -U "jax[cuda12]"
+pip install optax
+
 # pip install parsl
 # pip install -U "ray[data,train,tune,serve]"
 # pip install torch
